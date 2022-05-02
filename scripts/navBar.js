a48464fe4e155761445c901bfb541f53a879a421
Michael <tcmb139@gmail.com>
2022-05-02 12:12:36 -0400

    new navBar animation

--- scripts/navBar.js
+++ scripts/navBar.js
@@ -1,27 +1,23 @@
-// document.querySelector(".navBar").addEventListener("mouseover", () => {
-//     const timeline = gsap.timeline();
-//     timeline
-//         .to(document.querySelector(".navBar"), {
-//             duration: 0.5,
-//             width: "16rem",
-//         })
-//         .to(document.querySelector(".linkText"), {
-//             duration: 0.5,
-//             ease: CustomEase.create(
-//                 "custom",
-//                 "M0,0 C0.42,0 0.894,0.01 0.946,0.062 0.998,0.115 1,0.59 1,1 "
-//             ),
-//             opacity: 1,
-//         });
-// });
-//
-// document.querySelector(".navBar").addEventListener("mouseout", () => {
-//     const timeline = gsap.timeline();
-//     timeline
-//         .to(document.querySelector(".navBar"), {
-//             duration: 0.5,
-//             width: "5rem",
-//         })
-//         .to(document.querySelector(".linkText"), { duration: 0.5, opacity: 0 });
-// });
-//
+const navBar = document.querySelector(".navBar");
+
+let timeline = gsap.timeline({
+    default: { duration: 0.7, ease: "expo.inOut" },
+});
+
+navBar.addEventListener("mouseover", () => {
+    if (timeline.reversed()) {
+        timeline.play();
+    } else {
+        timeline
+            .to("nav", { width: "16rem" })
+            .to(
+                ".linkText",
+                { opacity: 1, pointerEvents: "all", stagger: 0.1 },
+                "-=0.4"
+            );
+    }
+});
+
+navBar.addEventListener("mouseout", () => {
+    timeline.reverse();
+});
