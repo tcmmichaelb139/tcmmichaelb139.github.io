67c198ee2cd22aa2d561fa65a5a619e82dba857e
Michael <tcmb139@gmail.com>
2022-05-02 11:12:04 -0400

    changed template

---
+++ scripts/navBar.js
@@ -0,0 +1,27 @@
+// document.querySelector(".navBar").addEventListener("mouseover", () => {
+//     const timeline = gsap.timeline();
+//     timeline
+//         .to(document.querySelector(".navBar"), {
+//             duration: 0.5,
+//             width: "16rem",
+//         })
+//         .to(document.querySelector(".linkText"), {
+//             duration: 0.5,
+//             ease: CustomEase.create(
+//                 "custom",
+//                 "M0,0 C0.42,0 0.894,0.01 0.946,0.062 0.998,0.115 1,0.59 1,1 "
+//             ),
+//             opacity: 1,
+//         });
+// });
+//
+// document.querySelector(".navBar").addEventListener("mouseout", () => {
+//     const timeline = gsap.timeline();
+//     timeline
+//         .to(document.querySelector(".navBar"), {
+//             duration: 0.5,
+//             width: "5rem",
+//         })
+//         .to(document.querySelector(".linkText"), { duration: 0.5, opacity: 0 });
+// });
+//
