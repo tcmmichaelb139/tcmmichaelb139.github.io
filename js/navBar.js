2a2fcf5712f92bafefaff65e4243ec48a2565acd
Michael <tcmb139@gmail.com>
2022-05-04 19:49:49 -0400

    theme change animation

--- js/navBar.js
+++ js/navBar.js
@@ -28,14 +28,34 @@ const body = document.body;
 
 const theme = localStorage.getItem("theme");
 
-if (body.classList.contains("dark") && theme === "light")
+let themeTl = gsap.timeline({
+    defaults: { duration: 1, ease: "expo.inOut" },
+});
+
+if (body.classList.contains("dark") && theme === "light") {
+    themeTl
+        .to("#themeButton .sun", { opacity: 0, rotate: "360deg" })
+        .to("#themeButton .moon", { opacity: 1, rotate: "360deg" }, "-=0.5");
     body.classList.replace("dark", "light");
+}
 
 themeButton.addEventListener("click", () => {
     if (body.classList.contains("light")) {
+        themeTl.reverse();
         body.classList.replace("light", "dark");
         localStorage.setItem("theme", "dark");
     } else {
+        if (themeTl.reversed()) {
+            themeTl.play();
+        } else {
+            themeTl
+                .to("#themeButton .sun", { opacity: 0, rotate: "360deg" })
+                .to(
+                    "#themeButton .moon",
+                    { opacity: 1, rotate: "360deg" },
+                    "-=0.5"
+                );
+        }
         body.classList.replace("dark", "light");
         localStorage.setItem("theme", "light");
     }
